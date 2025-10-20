e0960b509f7e6746051c707e4dcbec08730ed6fd
universe411 <haowang2411@gmail.com>
2025-10-20 14:49:10 -0700

    学了

--- 刷题/valid_anagram.py
+++ 刷题/valid_anagram.py
@@ -27,5 +27,17 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
         # s和t的长度要一样 然后每个字母的字数得一样 然后创建dictionary 查找？
-        if len(s) == len(t):
-            number_s = {}, number_t = {}
+        # needcode说确实要比较s和t的length长度 但他和我想法不一样
+        # #他是在最开始比较之后 如果不同直接return false
+        if len(s) != len(t):
+            return False
+
+        count_s = {}
+        count_t = {}
+
+        for i in range(len(s)):
+            # 用get，如果s[i]的value在countS里，那么就+1，如果没有，那么return default value 0
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[s[i]] = 1 + count_t.get(t[i], 0)
+
+        return count_s == count_t
